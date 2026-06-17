df1dc7c93e423f1af7d9e7cce0247f3cff63ef15
AhmedMedhat <75226176+AhmedMedhat77@users.noreply.github.com>
2026-06-17 22:47:39 +0300

    feat: implement responsive hero section layout, custom scrollbar, and statistics grid component

--- app/layouts/Header.tsx
+++ app/layouts/Header.tsx
@@ -3,7 +3,7 @@ import SectionCounter from "../components/section-counter";
 
 const Header = () => {
   return (
-    <div className="h-screen w-screen py-8 md:py-12 container mx-auto overflow-x-hidden border-l-primary border-l-12 px-6 md:px-8">
+    <div className="h-screen w-full py-8 md:py-12 border-l-primary border-l-12 px-6 md:px-8">
       {/* Header Section For Counter and Logo */}
       <div className="flex items-center justify-between">
         <h4 className="flex gap-1 text-white ">
@@ -14,19 +14,19 @@ const Header = () => {
       </div>
 
       {/* ====================== MAIN CONTENT ====================== */}
-      <div className="mt-14 md:mt-18  w-full relative grid grid-cols-3  ">
+      <div className="mt-14 md:mt-18  w-full grid grid-cols-1  md:grid-cols-3">
         {/* Left Content  */}
-        <div className="col-span-2 ">
+        <div className="col-span-1 md:col-span-2 md:static md:z-10">
           <h3 className="text-primary font-jet-brains-mono text-lg mb-8 md:mb-12">
             CONTENT TEAM LEAD · CREATIVE STRATEGIST
           </h3>
 
-          <h1 className="text-white text-8xl md:text-[12rem] leading-none font-bebas-neue  ">
+          <h1 className="text-white text-8xl md:text-[14rem] leading-none font-bebas-neue  ">
             AHMED <br /> ADHAM
             <span className="inline-block size-6 bg-primary" />
           </h1>
 
-          <p className="mt-10  font-dm-sans text-xl md:text-2xl  text-muted">
+          <p className="mt-10 font-dm-sans text-xl md:text-2xl  text-muted lg:max-w-[90%]">
             I don't make content.{" "}
             <span className="text-primary">
               I craft brands people talk about
@@ -34,15 +34,33 @@ const Header = () => {
             , and numbers worth remembering. Founder & CEO at KKA — 6+ years
             across fintech, F&B, healthcare, fashion and sports.
           </p>
-          <hr className="h-px border-0 bg-white/20 w-full my-12 " />
+          <hr className="h-px border-0 bg-white/20 w-full mt-12  mb-4" />
+
+          <div className="grid grid-cols-1 md:grid-cols-3">
+            <div className="header-statistics">
+              <p>USERS REACHED ACROSS BRANDS</p>
+              <span>50M+</span>
+            </div>
+            <div className="header-statistics">
+              <p>CLIENT SALES DRIVEN WITH ONE CLIENT</p>
+              <span>31M EGP</span>
+            </div>
+            <div className="header-statistics">
+              <p>BRANDS BUILT & MANAGED</p>
+              <span>40+</span>
+            </div>
+          </div>
         </div>
         {/* Right Content Image  */}
-        <div className="absolute -right-64 lg:w-[67%] h-full">
+        <div className="w-full md:w-full lg:w-[67%] md:absolute md:-right-40 lg:-right-90 md:-top-10 md:h-full z-10">
           <Image
             src={"/images/header.png"}
             alt={"Ahmed Adham"}
-            fill
-            className="object-contain absolute right-0"
+            width={800}
+            height={1200}
+            priority={true}
+            loading="eager"
+            className="w-full h-auto object-contain"
           />
         </div>
       </div>
